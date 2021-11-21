ce808c41be01a7e595999a2076aceeafe1d2ef7a
Costalferz <costalferzus@gmail.com>
2021-11-21 21:29:41 +0700

    test

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname +'/public/html/index.html'));
+  res.sendFile(path.join(__dirname,'public','/html/index.html'));
   //__dirname : It will resolve to your project folder.
 });
 
 //add the router
 app.use('/', router);
-app.listen(process.env.port || 3000);
+app.listen(process.env.port || 5000);
 app.use(express.static(path.join(__dirname, 'public')));
 
-console.log('Running at Port 3000');
+console.log('Running at Port 5000');
