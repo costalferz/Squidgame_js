3f8696f76baa13d286e4749d91f13c70ef3f74aa
Costalferz <costalferzus@gmail.com>
2021-11-21 22:36:57 +0700

    test

--- index.js
+++ index.js
@@ -12,9 +12,14 @@ router.get('/',function(req,res){
   //__dirname : It will resolve to your project folder.
 });
 
+router.get("/test", (req,res) => {
+  res.json("Hello AEIOU")
+})
 //add the router
 app.use('/', router);
 app.listen(process.env.port || 3000);
 app.use(express.static(path.join(__dirname, 'public')));
+
+//น่าสนใจ ลองเทสดู มันเข้าได้ปกติ  เข้าสักดิสสิ 555555
 
 console.log('Running at Port 3000');
